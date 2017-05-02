952119edd918666f2ec396d15c396a856daa8928
indra <indrasyahputra048@gmail.com>
2017-05-02 11:19:13 +0700

    Belajar include dan require once

---
+++ belajar/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sisfo Untan</title>
+</head>
+<body>
+	<h1>Sistem Informasi</h1>
+<?php 
+	include_once 'tabel.php';
+	require_once 'satu.php';
+ ?>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nihil, rem eligendi, quod architecto omnis eum sint repellat neque inventore deserunt pariatur odit consequatur. Quos nam perferendis eaque assumenda, ad. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, modi in cupiditate neque nisi veritatis perferendis ab aperiam id asperiores harum, magnam necessitatibus numquam, tempora itaque distinctio recusandae eius, sed? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur enim vero quia quo ad cumque neque delectus laborum expedita, libero a aliquid dicta, possimus consequatur, nemo in debitis, eveniet quos!</p>
+	<?php 
+	include_once 'tabel.php';
+	 ?>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,17 @@
+<table border="1"> 
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Nim</th>
+			<th>Nama</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>H1101151013</td>
+			<td>Indra</td>
+		</tr>
+	</tbody>
+
+</table>
