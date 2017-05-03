1cb51ff7efcebcdf4070d07794a1e8b779ac1b61
indra <indrasyahputra048@gmail.com>
2017-05-03 08:46:55 +0700

    Mengubah letak tag header dari sidebar.php ke header.php

--- administrator/template/header.php
+++ administrator/template/header.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <link href="template/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="template/assets/css/metisMenu.min.css" rel="stylesheet">
+    <link href="template/assets/css/sb-admin-2.css" rel="stylesheet">
+    <link href="template/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
 
     <div id="wrapper">
 
--- administrator/template/sidebar.php
+++ administrator/template/sidebar.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
-
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/metisMenu.min.css" rel="stylesheet">
-    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-</head>
 
 <body>
 
